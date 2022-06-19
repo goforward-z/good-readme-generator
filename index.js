895e561b4e4bdf03fcc2bf214477d1bc5eab7bfe
gofoward-z <zlepeng.ucf@gmail.com>
2022-06-19 16:14:01 -0400

    add validate to each user input

--- index.js
+++ index.js
@@ -10,33 +10,59 @@ const generateMarkdown = require('./utils/generateMarkdown');
 const questions = [
         {
             type: 'input',
-            name: 'project',
-            message: 'What is your project title?'
+            name: 'title',
+            message: 'What is your project title?',
+            validate: (value) => {
+                if (value) { return true } else { return 'Please enter your project title.' }
+            },
+        },
+        {
+            type: 'input',
+            name: 'repo',
+            message: 'What is the repository name of your project?',
+            validate: (value) => {
+                if (value) { return true } else { return 'Please enter your repository name.' }
+            },
         },
         {
             type: 'input',
             name: 'description',
             message: 'What is the description of your project?',
+            validate: (value) => {
+                if (value) { return true } else { return 'Please enter description.' }
+            },
         },
         {
             type: 'input',
             name: 'installation',
             message: 'Enter instructions for installation',
+            validate: (value) => {
+                if (value) { return true } else { return 'Please enter instructions.' }
+            },
         },
         {
             type: 'input',
             name: 'usage',
             message: 'What is the usage information?',
+            validate: (value) => {
+                if (value) { return true } else { return 'Please enter usage information.' }
+            },
         },
         {
             type: 'input',
             name: 'contribution',
             message: 'Is there any contribution you would like to add?',
+            validate: (value) => {
+                if (value) { return true } else { return 'Please enter your contribution guidelines.' }
+            },
         },
         {
             type: 'input',
             name: 'test',
             message: 'Any test to include?',
+            validate: (value) => {
+                if (value) { return true } else { return 'Please enter your test instructions.' }
+            },
         },
         {
             type: 'list',
@@ -48,11 +74,19 @@ const questions = [
             type: 'input',
             name: 'github',
             message: 'What is your GitHub username?',
+            validate: (value) => {
+                if (value) { return true } else { return 'Please choose a license.' }
+            },
         },
         {
             type: 'input',
             name: 'email',
             message: 'What is your email address?',
+            validate: (value) => {
+                if (emailValidator.validate(value)) {
+                    return true
+                } else { return 'Please enter a valid email address' }
+            },
         },
     ]
 
